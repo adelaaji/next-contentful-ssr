c70259bd66d9da26ea11a19825c291be99f2159e
Adel <adel.aaji@gmail.com>
2020-09-22 22:44:33 +0300

    ..

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -77,6 +77,6 @@ export async function getStaticPaths() {
   const allPosts = await getAllPostsWithSlug();
   return {
     paths: allPosts?.map(({ slug }) => `/posts/${slug}`) ?? [],
-    fallback: true,
+    fallback: false,
   };
 }
